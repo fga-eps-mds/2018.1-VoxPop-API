b2486fb40fdda0a5154a1a4d73b67eeba0898c9c
Lucas362 <lucasvitorifg@gmail.com>
2018-04-07 17:32:44 -0300

    Added social information tests.
    Co-authored-by: Eduardolimr <eduardolimrib@gmail.com>

--- api/tests.py
+++ api/tests.py
@@ -123,3 +123,138 @@ class UserTests(APITestCase):
             response.data,
             {'email': [_('Enter a valid email address.')]}
         )
+
+class SocialInformationTests(APITestCase):
+
+    def setUp(self):
+        """
+        This method will run before any test.
+        """
+
+        self.user = User.objects.create(
+            username='teste',
+            first_name='teste',
+            last_name='teste',
+            email='teste@teste.com',
+            password='teste'
+        )
+        self.social = SocialInformation.objects.create(
+            owner=self.user,
+            state='AC',
+            city='Rio Branco',
+            income='10.00',
+            education='EFC',
+            job='Dono de Casa',
+            birth_date='2018-04-07'
+        )
+        self.url = '/api/socialInformation/'
+
+    def tearDown(self):
+        """
+        This method will run after any test.
+        """
+        self.user.delete()
+        self.social.delete()
+
+    def test_create_social(self):
+        """
+        Ensure we can create a social information object.
+        """
+        response = self.client.get(self.url + str(self.social.pk) + '/')
+        new_social = SocialInformation.objects.get(pk=self.social.pk)
+        self.assertEqual(response.status_code,  status.HTTP_200_OK)
+
+    def test_invalid_create_social(self):
+        """
+        Ensure we can't create a invalid social information object.
+        """
+        data = {
+            "owner": self.user,
+            "state": "AC",
+            "city": "Rio Branco",
+            "income": "10.00",
+            "education": "EFC",
+            "job": "Dono de Casa",
+            "birth_date": "20180-32-13"
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_social(self):
+        """
+        Ensure we can update a new social information object.
+        """
+        self.assertEqual(self.social.job, 'Dono de Casa')
+        data = {
+            "owner": self.user.pk,
+            "state": "AC",
+            "city": "Rio Branco",
+            "income": "10.00",
+            "education": "EFC",
+            "job": "Dono de Prédio",
+            "birth_date": "2018-04-07"
+        }
+        response = self.client.put(self.url + str(self.social.pk) + '/', data)
+
+
+        new_social = SocialInformation.objects.get(pk=self.social.pk)
+        self.assertEqual(response.status_code,  status.HTTP_200_OK)
+        self.assertEqual(new_social.job, 'Dono de Prédio')
+
+    def test_invalid_update_social(self):
+        """
+        Ensure we can't update a social object with invalid fields.
+        """
+        self.assertEqual(self.social.birth_date, '2018-04-07')
+        data = {
+            "owner": self.user.pk,
+            "state": "AC",
+            "city": "Rio Branco",
+            "income": "10.00",
+            "education": "EFC",
+            "job": "Dono de Casa",
+            "birth_date": "20180-43-213"
+        }
+        response = self.client.put(self.url + str(self.social.pk) + '/', data)
+
+
+        new_social = SocialInformation.objects.get(pk=self.social.pk)
+        self.assertEqual(response.status_code,  status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(
+            response.data,
+            {"birth_date": [
+                "Date has wrong format. Use one of these formats instead: YYYY[-MM[-DD]]."
+            ]}
+        )
+
+    def test_partial_update_social(self):
+        """
+        Ensure we can partially update a social object.
+        """
+        self.assertEqual(self.social.job, 'Dono de Casa')
+        data = {
+            'job':'Dono de Condomínio',
+        }
+        response = self.client.patch(self.url + str(self.social.pk) + '/', data)
+        new_social = SocialInformation.objects.get(pk=self.social.pk)
+        self.assertEqual(response.status_code,  status.HTTP_200_OK)
+        self.assertEqual(new_social.job, 'Dono de Condomínio')
+
+    def test_invalid_partial_update_social(self):
+        """
+        Ensure we can't partially update invalid information on a valid social
+        object.
+        """
+        self.assertEqual(self.social.job, 'Dono de Casa')
+        data = {
+            'birth_date': '20180-56-89',
+        }
+        response = self.client.patch(self.url + str(self.social.pk) + '/', data)
+        new_social = SocialInformation.objects.get(pk=self.social.pk)
+        self.assertEqual(response.status_code,  status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(
+            response.data,
+            {"birth_date": [
+                "Date has wrong format. Use one of these formats instead: YYYY[-MM[-DD]]."
+            ]}
+        )
